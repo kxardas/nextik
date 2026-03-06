4b724e0553b0a31dd6f68c32fcbd4dea498f4ea7
Pavlo Halimurka <140071817+kxardas@users.noreply.github.com>
2026-03-06 11:03:54 +0100

    Minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -12,13 +12,18 @@ interface Props {
 export default async function Home({ searchParams }: Props) {
   const session = await getServerSession(authOptions);
 
-
   const resolvedSearchParams = await searchParams;
   const { login, logout, error } = resolvedSearchParams;
   return (
     <div>
       {login && <ToastNotification message='Successfully logged in' type='success' />}
       {logout && <ToastNotification message='Successfully logged out' type='success' />}
+
+      <main>
+        <p>username: {session?.user.name}</p>
+        <p>email: {session?.user.email}</p>
+        
+      </main>
     </div>
   );
 }
